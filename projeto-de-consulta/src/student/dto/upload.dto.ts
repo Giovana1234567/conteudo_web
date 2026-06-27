1f6f23f23a3475c099f992a1ae0e990114e6dcbd
suporte-bluescore <suporte@bluescore.com.br>
2026-06-27 19:48:32 -0300

    refactor: remover arquivos antigos em ingles pos-renomeacao

--- projeto-de-consulta/src/student/dto/upload.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-
-export class UploadDto {
-    @ApiProperty({ 
-        type: 'string', 
-        format: 'binary'
-    })
-    file: Express.Multer.File;
-}
